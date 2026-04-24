009966eb233efd66d24074c9197c40e9b85ba25e
kbanning04 <kbanning04@gmail.com>
2026-04-24 14:36:59 +0100

    Level 1 of Technical Assessment

--- Level-1/task_one.py
+++ Level-1/task_one.py
@@ -1,6 +1,63 @@
 from datetime import date
 
 
+class Trainee:
+    """ A Trainee at Tau Labs. """
+
+    def __init__(self, name: str, email: str, date_of_birth: date):
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments = []
+
+    def get_age(self) -> int:
+        """ Calculates how old a trainee is. """
+        date_of_birth = self.date_of_birth
+        current_date = date.today()
+        delta = (current_date - date_of_birth).days // 365.25
+        age = round(delta)
+        return age
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        """ Adds an assessment to the trainee's list of assessments. """
+        assessments = self.assessments
+        assessments.append(assessment)
+        return assessments
+
+    def get_assessment(self, name: str) -> Assessment | None:
+        """ Returns a specific assessment that has the name given. """
+        assessments = self.assessments
+        for test in assessments:
+            if name == test.name:
+                return test
+        return None
+
+
+class Assessment:
+    """ An assessment that a trainee should complete. """
+
+    def __init__(self, name: str, type: str, score: float):
+        self.name = name
+        self.type = type
+        self.type_check()
+        self.score = score
+        self.score_check()
+
+    def type_check(self):
+        """ Checks if a assessment is the right type: multiple-choice, technical or presentation."""
+        type = self.type
+        if type not in {"multiple-choice", "technical", "presentation"}:
+            raise ValueError(
+                "Assessment should be multiple-choice, technical or presentation only.")
+
+    def score_check(self):
+        """ Checks if the score is between 0 and 100. """
+        score = self.score
+        if score > 100:
+            raise ValueError("Score must be 100 or less")
+        elif score < 0:
+            raise ValueError("Score must be 0 or more.")
+
 
 if __name__ == "__main__":
     trainee = Trainee("Sigma", "trainee@sigmalabs.co.uk", date(1990, 1, 1))
